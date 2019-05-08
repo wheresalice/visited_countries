23fcc4d4bdd84e539240cc9a9c6f258aa1b8d5af
wheresalice <wheresalice@users.noreply.github.com>
2019-05-08 11:36:47 +0100

    use tcc_countries gem for printing changes

--- _bin/update_countries.rb
+++ _bin/update_countries.rb
@@ -1,22 +1,13 @@
-require 'nokogiri'
-require 'open-uri'
+#!/usr/bin/env ruby
 require 'yaml'
-
-
-page = Nokogiri::HTML(open('http://travelerscenturyclub.org/countries-and-territories'))
-
-# build list of countries from TCC webpage
-page_countries = []
-page.css("td[width='50%'] li").each do |country|
-  page_countries << country.text
-end
-page_countries.sort!
+require 'tcc_countries'
 
 # build list of countries from YAML file
 yaml_countries = YAML.safe_load(File.read('_data/countries.yaml')).map{|c| c['name']}
 
 puts '## newly added: ##'
-puts page_countries - yaml_countries
+new = TccCountries.all - yaml_countries
+puts new.map {|country| TccCountries::REGIONS.select{|region| TccCountries::COUNTRIES[region].include? country}[0] + ' :: ' + country}.sort
 
 puts '## removed: ##'
-puts yaml_countries - page_countries
+puts yaml_countries - TccCountries.all
